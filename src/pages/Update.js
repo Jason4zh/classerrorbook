f091c1c43f511da848e123caf04d17ce5b3102c1
Jason4zh <13640817984@163.com>
2025-09-20 15:39:20 +0800

    Upgrade Release 1.0.0

--- src/pages/Update.js
+++ src/pages/Update.js
@@ -2,8 +2,13 @@ const Update = () => {
   return (
     <div className="page update">
       <h2>这里是更新页面。请点击“主页”以开始。</h2>
+      <br />本网站已经可以正常使用啦~
       <br />新功能正在陆续添加中，敬请期待~
       <br />前面的区域以后再来探索吧~（雾
+      <br />欢迎在GitHub上提出建议和反馈~
+      <a href="github.com/your-repo" target="_blank" rel="noopener noreferrer">作者GitHub</a>
+      <br />感谢你的支持！
+      <br />by Jason4zh 15322313759@163.com
     </div>
   )
 }
