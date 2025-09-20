4d8df184c7a09819aa4b63f7b7153c7af5b816a9
Jason4zh <13640817984@163.com>
2025-09-20 15:35:38 +0800

    Update Update.js

--- src/pages/Update.js
+++ src/pages/Update.js
@@ -2,7 +2,8 @@ const Update = () => {
   return (
     <div className="page update">
       <h2>这里是更新页面。请点击“主页”以开始。</h2>
-目前搜索功能和图片上传暂未完工。 登录系统仅支持邮箱。 （注意！若使用登录系统，请务必在接受到登录链接后将其链接粘贴后更改localhost:3000为Jason4zh.github.io/classerrorbook并在浏览器中访问。） 前面的区域以后再来探索吧~（雾
+      <br />新功能正在陆续添加中，敬请期待~
+      <br />前面的区域以后再来探索吧~（雾
     </div>
   )
 }
